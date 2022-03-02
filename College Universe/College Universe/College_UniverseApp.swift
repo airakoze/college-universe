cc994d459ee2fc454d0da741d925cdd93cd424f7
Andy Florian Irakoze <irakoze.andy@gmail.com>
2022-03-02 10:51:09 -0600

    initial commit

---
+++ College Universe/College Universe/College_UniverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  College_UniverseApp.swift
+//  College Universe
+//
+//  Created by Andy Irakoze on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct College_UniverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
